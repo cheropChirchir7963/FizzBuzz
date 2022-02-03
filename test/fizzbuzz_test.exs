50c51065d0cb7e197013223750aa29c60731351d
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-02-03 17:07:12 +0300

    added test for number not divisble by 3 and 5

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -13,4 +13,9 @@ defmodule FizzbuzzTest do
   test "testing number divisible by 5" do
     assert Fizzbuzz.compute(5) == "buzz"
   end
+
+  test "testing number not divisible by 3 and 5" do
+    assert Fizzbuzz.compute(2) == 2
+    assert Fizzbuzz.compute(7) == 7
+  end
 end
