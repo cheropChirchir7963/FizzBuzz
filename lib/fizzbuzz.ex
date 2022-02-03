5fd99f08eac11e5d0ae62477da8edcef2399a06d
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-02-03 16:28:52 +0300

    added fizzbuzz output

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -4,15 +4,19 @@ defmodule Fizzbuzz do
   """
 
   @doc """
-  Hello world.
+  FizzBuzz
 
   ## Examples
 
-      iex> Fizzbuzz.hello()
-      :world
+      iex> Fizzbuzz.compute(3)
+      "fizz"
 
   """
-  def hello do
-    :world
+  def compute(num) when rem(num, 5) == 0 and rem(num, 3) == 0 do
+    "fizzbuzz"
   end
+
+  def compute(num) when rem(num, 3) == 0, do: "fizz"
+
+  def compute(num) when rem(num, 5) == 0, do: "buzz"
 end
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -2,7 +2,15 @@ defmodule FizzbuzzTest do
   use ExUnit.Case
   doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  test "testing number divisible by both 3 and 5" do
+    assert Fizzbuzz.compute(15) == "fizzbuzz"
+  end
+
+  test "testing number divisible by 3" do
+    assert Fizzbuzz.compute(3) == "fizz"
+  end
+
+  test "testing number divisible by 5" do
+    assert Fizzbuzz.compute(5) == "buzz"
   end
 end
